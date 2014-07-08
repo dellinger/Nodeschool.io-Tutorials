e824f61241dffdd5876064b9fefc7f251c70eac5
David Ellinger <david.ellinger@me.com>
2014-07-07 22:27:13 -0700

    Added comment to 10

--- 10/program.js
+++ 10/program.js
@@ -6,3 +6,10 @@ var server = net.createServer(function(socket){
    //socket handling logic
  });
  server.listen(port);
+
+
+ // Listen to TCP connection
+ // For each connection provide 24 hour time 
+ // "YYYY-MM-DD hh:mm"
+
+ // use socket.wrwite(data) to write data to the socket and socket.end() to close the socket. 
